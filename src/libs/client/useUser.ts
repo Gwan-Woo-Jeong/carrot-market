eda9678b7ed7e2a316aee54fe728a353e1cc6c05
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-06-22 00:17:57 +0900

    #10.3 SWR

--- src/libs/client/useUser.ts
+++ src/libs/client/useUser.ts
@@ -1,26 +1,34 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 /*
-  #10.2 useUser Hook
-  router.push() : 이전 페이지에 대한 히스토리를 남김
-  router.replace() : 남기지 않음 (히스토리 스택에 새 URL 추가 방지)
+  #10.3 SWR
+  Next.js 팀이 만든 React Data Fetching 라이브러리
+  먼저 캐시로부터 데이터를 반환한 후, fetch 요청(재검증)을 하고, 최종적으로 최신화된 데이터를 가져옴
+  컴포넌트가 자동으로 데이터 변경을 자동으로 감지, 이에 따라 UI를 빠르게 변경
+
+  npm i swr
+
+  사용법
+  1. 네이티브 fetch의 단순한 래퍼인 fetcher 함수를 생성
+  ex) const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+  2. useSWR을 import하고, 함수 컴포넌트 내에서 사용
+  ex) const { data, error } = useSWR('/api/user/123', fetcher)
+
+  일반적으로, 세 가지 요청 상태가 가능: 
+  "loading", "ready", "error". data와 error 값을 사용해 현재 요청의 상태를 알아내고, 해당하는 UI를 반환
  */
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function useUser() {
+  const { data, error } = useSWR("/api/users/me", fetcher);
   const [user, setUser] = useState();
   const router = useRouter();
 
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, []);
+  // return router.replace("/enter");
 
-  return user;
+  return data;
 }
