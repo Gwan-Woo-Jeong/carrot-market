732624f43021494b62f549d357a1e2b1f809dd92
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-06-28 17:53:32 +0900

    #12.7 ~ 12.8 Geo Search

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,11 +2,27 @@ import client from "@/libs/server/client";
 import { createResponse, getSession } from "@/libs/server/session";
 import { NextRequest, userAgent } from "next/server";
 
-// #12.5 All Posts
+// #12.7 Geo Search
+
+/*
+  parseFloat(string)
+  주어진 값을 필요한 경우 문자열로 변환한 후 부동소수점 실수로 파싱해 반환
+  ex) parseFloat('3.14'); // 3.14
+
+  gte: 크거나 같다.
+  lte: 작거나 같다.
+
+  const getPosts = await prisma.post.findMany({
+  where: { likes: { lte: 9 } }
+  })
+ */
 
 export async function GET(req: NextRequest) {
   const res = new Response();
   const session = await getSession(req, res);
+
+  const latitude = req.nextUrl.searchParams.get("latitude");
+  const longitude = req.nextUrl.searchParams.get("longitude");
 
   if (session.user) {
     const posts = await client.post.findMany({
@@ -24,6 +40,21 @@ export async function GET(req: NextRequest) {
             answers: true,
           },
         },
+      },
+      ...{
+        ...(latitude &&
+          longitude && {
+            where: {
+              latitude: {
+                gte: parseFloat(latitude.toString()) - 0.01,
+                lte: parseFloat(latitude.toString()) + 0.01,
+              },
+              longitude: {
+                gte: parseFloat(longitude.toString()) - 0.01,
+                lte: parseFloat(longitude.toString()) + 0.01,
+              },
+            },
+          }),
       },
     });
 
--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import FloatingButton from "../../components/floating-button";
 import useSWR from "swr";
 import { Post, User } from "@prisma/client";
+import useCoords from "@/libs/client/useCoords";
 
 interface PostResponse {
   ok: boolean;
@@ -20,10 +21,15 @@ interface PostWithUser extends Post {
   };
 }
 
-// #12.5 All Posts
+// #12.7 Geo Search
 
 const Community: NextPage = () => {
-  const { data } = useSWR<PostResponse>(`/api/posts`);
+  const { latitude, longitude } = useCoords();
+  const { data } = useSWR<PostResponse>(
+    latitude && longitude
+      ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
+      : null
+  );
 
   return (
     <Layout hasTabBar title="동네생활">
