b14bd83604404197b93df243ea1b079545a1c321
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-07-04 18:07:27 +0900

    #15 CLOUDFLARE IMAGES | #15.1 Image Preview

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -6,9 +6,10 @@ import Input from "@/components/input";
 import Layout from "@/components/layout";
 import useUser from "@/libs/client/useUser";
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useMutation from "@/libs/client/useMutation";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 // #13.6 Edit Profile
 
@@ -16,6 +17,7 @@ interface EditProfileForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -32,6 +34,7 @@ const EditProfile: NextPage = () => {
     handleSubmit,
     setError,
     formState: { errors },
+    watch,
   } = useForm<EditProfileForm>();
 
   const [editProfile, { data, loading }] =
@@ -57,7 +60,7 @@ const EditProfile: NextPage = () => {
     }
   }, [data, router]);
 
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
@@ -67,17 +70,50 @@ const EditProfile: NextPage = () => {
     editProfile({ email, phone, name });
   };
 
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const avatar = watch("avatar");
+
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0]; // 브라우저의 메모리에 있는 파일 (URL로 접근 불가능)
+      setAvatarPreview(URL.createObjectURL(file)); // URL 접근 가능
+    }
+  }, [avatar]);
+
+  /*
+  #15.1 Image Preview
+
+  URL.createObjectURL(object)
+  - object : 객체 URL을 생성할 File, Blob, MediaSource 객체
+  - 리턴값 : 지정한 object의 참조 URL을 담은 DOMString
+
+  - 주어진 객체를 가리키는 URL을 DOMString으로 반환
+  - 같은 객체를 사용하더라도, 매번 호출할 때마다 새로운 객체 URL을 생성
+  - 각각의 URL을 더는 쓰지 않을 땐 URL.revokeObjectURL()을 사용해 하나씩 해제해줘야 함
+   */
+
   return (
     <Layout canGoBack title="Edit Profile">
       <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <Image
+              width={14}
+              height={14}
+              alt="avatar-preview"
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
