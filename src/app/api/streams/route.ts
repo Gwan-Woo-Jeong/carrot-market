8566db0619280f395b71450f035ff472e98c0202
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-07-07 23:36:52 +0900

    #17 LIVE STREAMING | #17.2 Streaming API

--- src/app/api/streams/route.ts
+++ src/app/api/streams/route.ts
@@ -43,17 +43,63 @@ export async function GET(req: NextRequest) {
   }
 }
 
+// #17.2 Stream API
+/*
+  실시간 스트림 시작
+
+  API를 통해 라이브 스트림을 시작하려면 /live_inputs 엔드포인트에 POST 요청
+
+  mode : automatic
+  - 라이브 스트림을 자동으로 볼 수 있음
+  - 종료 후 다시 볼 수 있도록 자동으로 녹화
+
+  body 속성
+  timeoutSeconds
+  - 라이브 피드의 연결을 끊을 수 있는 시간을 지정. 10초라면 10초 간 스트리머와 연결이 끊길 시, 라이브 자동 종료.
+
+  requireSignedURLs
+  - 비디오를 보기 위해 서명된 URL이 필요한지 여부
+
+  allowedOrigins
+ - 선택적으로 호출되어 허용된 출처 목록을 제공 가능
+ */
+
 export async function POST(req: NextRequest) {
   const res = new Response();
   const { name, price, description } = await req.json();
   const session = await getSession(req, res);
 
   if (session.user) {
+    /*
+    Cloud Flare Live Stream
+
+    const {
+      result: {
+        uid,
+        rtmps: { streamKey, url },
+      },
+    } = await (
+      await fetch(
+        `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/stream/live_inputs`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.CF_STREAM_TOKEN}`,
+          },
+          body: `{"meta": {"name":"${name}"},"recording": { "mode": "automatic", "timeoutSeconds": 10 }}`,
+        }
+      )
+    ).json();
+    */
+
     const stream = await client.stream.create({
       data: {
         name,
         price: +price,
         description,
+        // cloudflareId: uid,
+        // cloudflareKey: streamKey,
+        // cloudflareUrl: url,
         user: {
           connect: { id: session.user.id },
         },
