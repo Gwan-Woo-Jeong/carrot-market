b78e8eedb927579082a6addd2ade2f4e7450438c
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-07-10 16:37:50 +0900

    #19.5 Script Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import { SWRConfig } from "swr";
 import React from "react";
 import { fetcher } from "@/libs/client/utils";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,22 @@ const inter = Inter({ subsets: ["latin"] });
   const { data, error, isValidating, mutate } = useSWR(key, fetcher, options)
  */
 
+/*
+  #19.5 Script Component
+
+  - HTML `script` 태그의 확장
+  - 앱에서 써드 파티 스크립트의 로드되는 우선 순위를 설정, 개발 시간을 절약하면서 로드하는 성능을 향상 가능
+
+  strategy options
+  - beforeInteractive : 페이지가 interactive 되기 전에 로드 
+  - afterInteractive (default) : 페이지가 interactive 된 후에 로드
+  - lazyOnload : 다른 모든 데이터나 소스를 불러온 후에 로드
+  - worker (experimental) : web worker에 로드
+
+  onLoad prop
+  - `Script` 에 명시된 스크립트를 다 불러온 다음에 실행
+ */
+
 export default function RootLayout({
   children,
 }: {
@@ -29,6 +46,23 @@ export default function RootLayout({
       <body className={inter.className}>
         <SWRConfig value={{ fetcher }}>
           <div className="w-full max-w-lg mx-auto">{children}</div>
+          {/* <Script
+            src="https://developers.kakao.com/sdk/js/kakao.js"
+            strategy="lazyOnload"
+          />
+          <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            onLoad={() => {
+              window.fbAsyncInit = function () {
+                FB.init({
+                  appId: "your-app-id",
+                  autoLogAppEvents: true,
+                  xfbml: true,
+                  version: "v13.0",
+                });
+              };
+            }}
+          /> */}
         </SWRConfig>
       </body>
     </html>
