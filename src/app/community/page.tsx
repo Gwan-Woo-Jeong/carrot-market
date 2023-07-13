c44f5dc1b260a7cffeb45232b11d9507f77dcc8c
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-07-13 12:18:06 +0900

    #20 INCREMENTAL SITE REGENERATION | #20.1 ~ 20.2 ISR

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -7,6 +7,7 @@ import FloatingButton from "../../components/floating-button";
 import useSWR from "swr";
 import { Post, User } from "@prisma/client";
 import useCoords from "@/libs/client/useCoords";
+import { use } from "react";
 
 interface PostResponse {
   ok: boolean;
@@ -21,15 +22,46 @@ interface PostWithUser extends Post {
   };
 }
 
+// #20.1 ~ 20.2 ISR
+/*
+  사이트를 build한 후 다시 static 페이지를 만들거나 업데이트
+  전체 사이트가 아닌 각 페이지별로 정적으로 페이지 (static-generation)를 생성 
+  수백만 페이지로 확장하면서 static의 이점을 유지 가능
+
+  getStaticProps에 revalidate prop을 추가 (= ISR)
+  
+  revalidate : 백그라운드(서버)에서 페이지를 재생성(rebuild)하고 캐싱하고 있는 시간 (최소 10초)
+
+  이 시간이 지난 후, 요청이 올 경우 해당 요청은 여전히 캐시된 페이지를 응답
+  그 다음 페이지를 재생성하고 또 다음 revalidate 시간동안 캐싱
+
+  ISR을 사용하지 못하는 경우
+  - 유저마다 다른 정보나 UI를 보여주는 페이지인 경우
+ */
+
+const fetchPosts = async () => {
+  const res = await fetch("/api/posts", {
+    next: {
+      revalidate: 10, // 10초마다 static 페이지 최신화 (getStaticProps) = ISR (Incremental Static Regeneration)
+    },
+  });
+
+  return res.json();
+};
+
+const postsPromise = fetchPosts();
+
 // #12.7 Geo Search
 
 const Community: NextPage = () => {
-  const { latitude, longitude } = useCoords();
-  const { data } = useSWR<PostResponse>(
-    latitude && longitude
-      ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
-      : null
-  );
+  // const { latitude, longitude } = useCoords();
+  // const { data } = useSWR<PostResponse>(
+  //   latitude && longitude
+  //     ?  `/api/posts?latitude=${latitude}&longitude=${longitude}`
+  //     : null
+  // );
+
+  const data: PostResponse = use(postsPromise);
 
   return (
     <Layout hasTabBar title="동네생활">
