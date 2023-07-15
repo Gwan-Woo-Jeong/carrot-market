54fb5bd4ed7e2ae9cbadac1194d0690891d74e9d
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-07-15 13:56:58 +0900

    #21.4 Parallelism

--- src/app/coins/page.tsx
+++ src/app/coins/page.tsx
@@ -11,36 +11,61 @@
     - Suspense는 내부의 컴포넌트의 fetch 함수가 Promise를 반환하면 성공/실패를 확인한 후 컴포넌트를 렌더링
  */
 
-// #12.3 Throwing Our Promise
+/*
+    #21.4 Parallelism
+    - 서버에 의해 모든 컴포넌트가 평행적으로 렌더링
+    - 모든 컴포넌트들이 다른 컴포넌트가 렌더링되기까지 기다릴 필요 X
+    - http 스트리밍을 통해 리액트가 결과를 받으면 Suspense fallback에 따라 결과를 보여줌
+ */
 
 import { Suspense } from "react";
 import RootLayout from "./layout";
-
-let finished = false;
 
 const cache: any = {};
 
 function fetchData(url: string) {
   if (!cache[url]) {
-    throw fetch(url)
-      .then((r) => r.json())
-      .then((json) => (cache[url] = json.slice(0, 10)));
+    throw Promise.all([
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => (cache[url] = json)),
+      new Promise((resolve) =>
+        setTimeout(resolve, Math.round(Math.random() * 10555))
+      ),
+    ]);
   }
 
   return cache[url];
+}
+function Coin({ id, name, symbol }: any) {
+  const {
+    quotes: {
+      USD: { price },
+    },
+  } = fetchData(`https://api.coinpaprika.com/v1/tickers/${id}`);
+  return (
+    <span>
+      {name} / {symbol}: ${price}
+    </span>
+  );
 }
 
 function List() {
   const coins = fetchData("https://api.coinpaprika.com/v1/coins");
 
   return (
-    <ul>
-      {coins.map((coin: any) => (
-        <li key={coin.id}>
-          {coin.name} / {coin.symbol}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h4>List is done</h4>
+      <ul>
+        {coins.slice(0, 10).map((coin: any) => (
+          <li key={coin.id}>
+            <Suspense fallback={`Coin ${coin.name} is loading`}>
+              <Coin {...coin} />
+            </Suspense>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
