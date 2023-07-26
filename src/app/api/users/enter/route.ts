99dff877690579ac02d5e39487e65d37953a4d1a
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-07-26 14:04:51 +0900

    Fix disable twilio

--- src/app/api/users/enter/route.ts
+++ src/app/api/users/enter/route.ts
@@ -1,14 +1,14 @@
 import client from "@/libs/server/client";
 import { NextRequest, NextResponse } from "next/server";
-import twilio from "twilio";
-import mail from "@sendgrid/mail";
+// import twilio from "twilio";
+// import mail from "@sendgrid/mail";
 
 /*
   9.5 Sending Email
  */
 
-mail.setApiKey(process.env.SENDGRID_API_KEY!);
-const twilioClienet = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+// mail.setApiKey(process.env.SENDGRID_API_KEY!);
+// const twilioClienet = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 export async function POST(req: NextRequest) {
   const { phone, email } = await req.json();
@@ -36,24 +36,24 @@ export async function POST(req: NextRequest) {
   });
 
   // 핸드폰 번호 가입인 경우 token을 문자로 전송
-  if (phone) {
-    // const message = await twilioClienet.messages.create({
-    //   messagingServiceSid: process.env.TWILIO_MSID,
-    //   to: process.env.MY_PHONE!,
-    //   body: `Your login token is ${payload}`,
-    // });
-    // console.log(message);
-    // 이메일 가입인 경우 token을 이메일로 전송
-  } else if (email) {
-    // const email = await mail.send({
-    //   from: "19c9d4@naver.com",
-    //   to: "19c9d4@naver.com",
-    //   subject: "Your Carrot Market Verification Email",
-    //   text: `Your login token is ${payload}`,
-    //   html: `<strong>Your login token is ${payload}</strong>`,
-    // });
-    // console.log(email);
-  }
+  // if (phone) {
+  // const message = await twilioClienet.messages.create({
+  //   messagingServiceSid: process.env.TWILIO_MSID,
+  //   to: process.env.MY_PHONE!,
+  //   body: `Your login token is ${payload}`,
+  // });
+  // console.log(message);
+  // 이메일 가입인 경우 token을 이메일로 전송
+  // } else if (email) {
+  // const email = await mail.send({
+  //   from: "19c9d4@naver.com",
+  //   to: "19c9d4@naver.com",
+  //   subject: "Your Carrot Market Verification Email",
+  //   text: `Your login token is ${payload}`,
+  //   html: `<strong>Your login token is ${payload}</strong>`,
+  // });
+  // console.log(email);
+  // }
 
   return NextResponse.json({ ok: true, token });
 }
